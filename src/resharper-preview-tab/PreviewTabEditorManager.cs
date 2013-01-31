523b024870da15e3b95e9ddaa6af66cb0dd4272d
Matt Ellis <m.t.ellis@gmail.com>
2013-01-31 13:43:30 +0000

    Initial commit

---
+++ src/resharper-preview-tab/PreviewTabEditorManager.cs
@@ -0,0 +1,49 @@
+﻿using JetBrains.DataFlow;
+using JetBrains.DocumentManagers;
+using JetBrains.DocumentModel.Transactions;
+using JetBrains.IDE;
+using JetBrains.ProjectModel;
+using JetBrains.TextControl;
+using JetBrains.UI.WindowManagement;
+using JetBrains.VsIntegration.DevEleven;
+using JetBrains.VsIntegration.DocumentModel;
+using JetBrains.VsIntegration.ProjectModel;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace resharper_preview_tab
+{
+    [SolutionComponent]
+    public class PreviewTabEditorManager : VSEditorManager11 
+    {
+        private readonly DocumentTransactionManager documentTransactionManager;
+
+        public PreviewTabEditorManager(Lifetime lifetime, ProjectModelSynchronizer projectModelSynchronizer,
+                                       IVsUIShellOpenDocument vsUiShellOpenDocument,
+                                       VsDocumentManagerSynchronization vsDocumentManagerSynchronization,
+                                       ITextControlManager textControlManager, IFrameFocusHelper frameFocusHelper,
+                                       DocumentManager documentManager, DocumentTransactionManager documentTransactionManager)
+            : base(lifetime, projectModelSynchronizer, vsUiShellOpenDocument, vsDocumentManagerSynchronization,
+                   textControlManager, frameFocusHelper, documentManager)
+        {
+            this.documentTransactionManager = documentTransactionManager;
+        }
+
+        protected override bool OpenInProvisionTab(TabOptions tabOptions)
+        {
+            // If we're in a transaction, something's happening (e.g. opening files for a refactoring)
+            // so don't force the preview tab. If we're not in a transaction, that just means someone's
+            // navigating to the file, so yes, force the preview tab
+            return IsInDocumentTransaction ? base.OpenInProvisionTab(tabOptions) : ShouldUsePreviewTab(tabOptions);
+        }
+
+        private bool IsInDocumentTransaction
+        {
+            get { return documentTransactionManager.CurrentTransaction != null; }
+        }
+
+        private bool ShouldUsePreviewTab(TabOptions tabOptions)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/resharper-preview-tab/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using JetBrains.Application.PluginSupport;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("resharper-preview-tab")]
+[assembly: AssemblyDescription("Controls ReSharper's use of Visual Studio's preview tab")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Matt Ellis")]
+[assembly: AssemblyProduct("resharper-preview-tab")]
+[assembly: AssemblyCopyright("Copyright © Matt Ellis, 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// The following information is displayed by ReSharper in the Plugins dialog
+[assembly: PluginTitle("Preview Tab Behaviour")]
+[assembly: PluginDescription("Controls ReSharper's use of Visual Studio's preview tab")]
+[assembly: PluginVendor("Matt Ellis")]
