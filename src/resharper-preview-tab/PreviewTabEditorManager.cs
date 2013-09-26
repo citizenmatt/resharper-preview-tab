27b0b4a318a7c55ebfa28359a51270c7577aa777
Matt Ellis <m.t.ellis@gmail.com>
2013-09-26 15:19:54 +0200

    Works with ReSharper 8's multi-file templates

--- src/resharper-preview-tab/PreviewTabEditorManager.cs
+++ src/resharper-preview-tab/PreviewTabEditorManager.cs
@@ -14,13 +14,16 @@
  * limitations under the License.
  */
 
+using System;
 using JetBrains.DataFlow;
 using JetBrains.DocumentManagers;
 using JetBrains.DocumentModel.Transactions;
 using JetBrains.IDE;
 using JetBrains.ProjectModel;
 using JetBrains.TextControl;
+using JetBrains.Threading;
 using JetBrains.UI.WindowManagement;
+using JetBrains.Util;
 using JetBrains.VsIntegration.DocumentModel;
 using JetBrains.VsIntegration.ProjectModel;
 using Microsoft.VisualStudio.Shell.Interop;
@@ -30,17 +33,30 @@ namespace CitizenMatt.ReSharper.PreviewTab
     [SolutionComponent]
     public partial class PreviewTabEditorManager
     {
+        // Defined in Microsoft.VisualStudio.Shell.11.0.dll, but this saves us having to reference it
+        // (and reference Microsoft.VisualStudio.Shell.12.0.dll for VS2013)
+        private static readonly Guid NavigationReason = new Guid("8d57e022-9e44-4efd-8e4e-230284f86376");
+
+        private readonly IVsUIShellOpenDocument3 vsUiShellOpenDocument3;
         private readonly DocumentTransactionManager documentTransactionManager;
+        private readonly IThreading threading;
+        private IVsNewDocumentStateContext newDocumentStateContext;
+        private Action doEnablePreviewTab;
 
         public PreviewTabEditorManager(Lifetime lifetime, ProjectModelSynchronizer projectModelSynchronizer,
                                        IVsUIShellOpenDocument vsUiShellOpenDocument,
                                        VsDocumentManagerSynchronization vsDocumentManagerSynchronization,
                                        ITextControlManager textControlManager, IFrameFocusHelper frameFocusHelper,
-                                       DocumentManager documentManager, DocumentTransactionManager documentTransactionManager)
+                                       DocumentManager documentManager, DocumentTransactionManager documentTransactionManager,
+                                       IThreading threading)
             : base(lifetime, projectModelSynchronizer, vsUiShellOpenDocument, vsDocumentManagerSynchronization,
                    textControlManager, frameFocusHelper, documentManager)
         {
+            vsUiShellOpenDocument3 = vsUiShellOpenDocument as IVsUIShellOpenDocument3;
             this.documentTransactionManager = documentTransactionManager;
+            this.threading = threading;
+
+            doEnablePreviewTab = DoEnablePreviewTab;
         }
 
         protected override bool OpenInProvisionTab(TabOptions tabOptions)
@@ -48,7 +64,27 @@ namespace CitizenMatt.ReSharper.PreviewTab
             // If we're in a transaction, something's happening (e.g. opening files for a refactoring)
             // so don't force the preview tab. If we're not in a transaction, that just means someone's
             // navigating to the file, so yes, force the preview tab
-            return IsInDocumentTransaction ? base.OpenInProvisionTab(tabOptions) : ShouldUsePreviewTab(tabOptions);
+            return !IsInDocumentTransaction;
+        }
+
+        protected override void EnablePreviewTab()
+        {
+            doEnablePreviewTab();
+        }
+
+        public override ITextControl OpenProjectFile(IProjectFile projectFile, bool activate, FileView fileViewPrimary,
+            TabOptions tabOptions = TabOptions.Default)
+        {
+            var textControl = base.OpenProjectFile(projectFile, activate, fileViewPrimary, tabOptions);
+            RestoreNewDocumentStateContext();
+            return textControl;
+        }
+
+        public override ITextControl OpenFile(FileSystemPath fileName, bool activate, TabOptions tabOptions)
+        {
+            var textControl = base.OpenFile(fileName, activate, tabOptions);
+            RestoreNewDocumentStateContext();
+            return textControl;
         }
 
         private bool IsInDocumentTransaction
@@ -56,9 +92,41 @@ namespace CitizenMatt.ReSharper.PreviewTab
             get { return documentTransactionManager.CurrentTransaction != null; }
         }
 
-        private bool ShouldUsePreviewTab(TabOptions tabOptions)
+        private void DoEnablePreviewTab()
         {
-            return true;
+            if (vsUiShellOpenDocument3 == null)
+                return;
+
+            SetNewDocumentState();
+            DisablePreviewTabUntilIdle();
+        }
+
+        private void SetNewDocumentState()
+        {
+            newDocumentStateContext = vsUiShellOpenDocument3.SetNewDocumentState((uint)__VSNEWDOCUMENTSTATE.NDS_Provisional, NavigationReason);
+        }
+
+        // We can only open one document in the preview tab. Subsequent documents need to use normal tabs.
+        // Enqueue a re-enable command with the UI - once the UI is executing again, the preview tab has
+        // been shown, and it's fair game again. This fixes issues with multi-file templates
+        private void DisablePreviewTabUntilIdle()
+        {
+            doEnablePreviewTab = () => { };
+            threading.Dispatcher.BeginInvoke("reset preview tab", () => doEnablePreviewTab = DoEnablePreviewTab);
+        }
+
+        // Make sure we restore the document state, or VS will try to open subsequent documents in the preview
+        // tab (ideally, this should happen in VsEditorManager, which calls SetNewDocumentState). Incidentally,
+        // I don't know why we don't need to reset this when using to open files one at a time, but we do need
+        // to use it when we try to open multiple files at once (e.g. multi-file templates). Perhaps the GC
+        // kicks in and frees the early object for us? Or there's a timeout?
+        private void RestoreNewDocumentStateContext()
+        {
+            if (newDocumentStateContext != null)
+            {
+                newDocumentStateContext.Restore();
+                newDocumentStateContext = null;
+            }
         }
     }
 }
